fb8c8e4de6acb5e249d6271a5326365a57a42c48
st140332 <bjiagu4e@gmail.com>
2018-04-11 16:40:03 +0300

    Location,Date,Light without DB

--- app/src/main/java/com/vlad/location/MainActivity.java
+++ app/src/main/java/com/vlad/location/MainActivity.java
@@ -4,6 +4,10 @@ import android.Manifest;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -26,11 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnLoc;
     LocationManager locationManager;
+    SensorManager sensorManager;
+    Sensor sensorLight;
+    String level;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         btnLoc = (Button) findViewById(R.id.btnGetLoc);
         ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 123);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -42,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                sensorManager.registerListener(listenerLight, sensorLight,
+                        SensorManager.SENSOR_DELAY_NORMAL);
                 GpsTracker gt = new GpsTracker(getApplicationContext());
                 Location l = gt.getLocation();
                 if (l == null)
@@ -53,7 +64,8 @@ public class MainActivity extends AppCompatActivity {
                     String date = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault()).format(new Date());
                     double lat = l.getLatitude();
                     double lon = l.getLongitude();
-                    Toast.makeText(getApplicationContext(), "GPS Lat = " + lat + "\n lon = " + lon + " \n Date and time: " + date, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "GPS Lat = " + lat + "\n lon = "
+                            + lon + " \n Date and time: " + date + " \n Light: " + level, Toast.LENGTH_SHORT).show();
                 }
 
              }
@@ -77,4 +89,19 @@ public class MainActivity extends AppCompatActivity {
         final AlertDialog alert = builder.create();
         alert.show();
     }
+
+    SensorEventListener listenerLight = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+           // tvText.setText(String.valueOf(event.values[0]));
+             level = String.valueOf(event.values[0]);
+
+
+        }
+    };
 }
