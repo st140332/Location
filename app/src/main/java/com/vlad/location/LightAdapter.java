b75d088feea35d9646258cbfb56bcbf138ae7d93
st140332 <bjiagu4e@gmail.com>
2018-04-17 15:01:13 +0300

    lat,lon to Map

--- app/src/main/java/com/vlad/location/LightAdapter.java
+++ app/src/main/java/com/vlad/location/LightAdapter.java
@@ -1,10 +1,13 @@
 package com.vlad.location;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,11 +32,18 @@ public class LightAdapter extends RecyclerView.Adapter<LightAdapter.ViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(LightAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(LightAdapter.ViewHolder holder, final int position) {
         holder.lat.setText("Latitude: "+Double.toString(lights.get(position).getLatitude()));
         holder.lon.setText("Longitude: "+Double.toString(lights.get(position).getLongitude()));
         holder.light.setText("lx: "+Double.toString(lights.get(position).getLight()));
         holder.date.setText(lights.get(position).getCreatedAt());
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:<" + lights.get(position).getLatitude()  + ">,<" + lights.get(position).getLongitude() + ">?q=<" + lights.get(position).getLatitude()  + ">,<" + lights.get(position).getLongitude() + ">(Searched place)"));
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -46,12 +56,14 @@ public class LightAdapter extends RecyclerView.Adapter<LightAdapter.ViewHolder> 
         public TextView lon;
         public TextView light;
         public TextView date;
+        public RelativeLayout relativeLayout;
         public ViewHolder(View itemView) {
             super(itemView);
             lat = (TextView) itemView.findViewById(R.id.lat);
             lon = (TextView) itemView.findViewById(R.id.lon);
             light = (TextView) itemView.findViewById(R.id.light);
             date = (TextView) itemView.findViewById(R.id.date);
+            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relative_layout);
         }
     }
 }
